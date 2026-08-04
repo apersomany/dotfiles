0ca937df7ec96527aa711e5476df3b58a0888dc6
apersomany <aperso@aperso.dev>
2026-08-04 14:41:08 +0900

    Revert "feat(noctalia): track color and plugin overrides"
    This reverts commit 7972c6411565ec53a37969889acba8524f369a11.

--- modules/desktop/noctalia.nix
+++ modules/desktop/noctalia.nix
@@ -3,12 +3,6 @@ let
   noctaliaConfig = pkgs.writeText "noctalia-config.toml" (
     builtins.readFile ../../files/noctalia/config.toml
   );
-  noctaliaColors = pkgs.writeText "noctalia-colors.json" (
-    builtins.readFile ../../files/noctalia/colors.json
-  );
-  noctaliaPlugins = pkgs.writeText "noctalia-plugins.json" (
-    builtins.readFile ../../files/noctalia/plugins.json
-  );
 in
 {
   programs.noctalia = {
@@ -19,7 +13,5 @@ in
 
   systemd.user.tmpfiles.rules = [
     "L+ %h/.config/noctalia/config.toml - - - - ${noctaliaConfig}"
-    "L+ %h/.config/noctalia/colors.json - - - - ${noctaliaColors}"
-    "L+ %h/.config/noctalia/plugins.json - - - - ${noctaliaPlugins}"
   ];
 }
